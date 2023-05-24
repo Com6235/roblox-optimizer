10a73a0f7ee17b8e2c0e17a98e6c04981f4f73be
Com6235 <ru.nms199@gmail.com>
2023-05-24 22:47:34 +0300

    Filesystem try

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const os = require("path/win32")
-os.
+const { log } = require('console')
+const fs = require('fs')
+const path = require('path')
+const folderPath = '/Users/User'
+
+fs.readdirSync(folderPath).map(fileName => {
+    log(path.join(folderPath, fileName))
+    return path.join(folderPath, fileName)
+  })
