5a860928247e159de804df074196295373cdfd60
Com6235 <ru.nms199@gmail.com>
2023-05-31 23:30:01 +0300

    made an win forms app

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace WindowsFormsApp1
             this.label1 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -64,12 +65,22 @@ namespace WindowsFormsApp1
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Test Text";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(243, 279);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
@@ -85,6 +96,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
@@ -14,9 +15,12 @@ namespace WindowsFormsApp1
     {
         public bool enableOptimizer = false;
 
+        public string settings = System.IO.File.ReadAllText("settings.json");
+
         public Form1()
         {
             InitializeComponent();
+            label2.Text = "";
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
@@ -28,12 +32,117 @@ namespace WindowsFormsApp1
         {
             if (enableOptimizer == true)
             {
-                Console.WriteLine("true");
+                string mainDirPath = System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\AppData\\Local\\Roblox\\Versions"; ;
+                DirectoryInfo mainDir = new DirectoryInfo(mainDirPath);
+                DirectoryInfo[] subDirs = mainDir.GetDirectories();
+                DateTime lastCreated = DateTime.MinValue;
+                DirectoryInfo lastCreatedDir = null;
+
+                label2.Text = "Finding the last version folder...";
+
+                foreach (DirectoryInfo subDir in subDirs)
+                {
+                    if (subDir.CreationTime > lastCreated)
+                    {
+                        lastCreated = subDir.CreationTime;
+                        lastCreatedDir = subDir;
+                    }
+                }
+
+                if (lastCreatedDir != null)
+                {
+                    Console.WriteLine("Last created directory: " + lastCreatedDir.FullName);
+                }
+
+                List<string> subDirsOfLastCreatedDir = new List<string>();
+                foreach (System.IO.DirectoryInfo dir in lastCreatedDir.EnumerateDirectories())
+                {
+                    subDirsOfLastCreatedDir.Add(dir.Name);
+                    // Console.WriteLine(subDirsOfLastCreatedDir.Count);
+                }
+
+                if (subDirsOfLastCreatedDir.Contains("ClientSettings")) 
+                {
+                    label2.Text = "ClientSettings was found! Checking for the file...";
+
+                    Console.WriteLine("ClientSettings was found! Checking for the file...");
+                    List<string> yops = new List<string>();
+                    foreach (string yop in System.IO.Directory.EnumerateFiles(lastCreatedDir.FullName + "\\ClientSettings"))
+                    {
+                        yops.Add(yop);
+                        Console.WriteLine(yop);
+                    }
+                    if (yops.Contains(lastCreatedDir.FullName + "\\ClientSettings\\" + "ClientAppSettings.json"))
+                    {
+                        label2.Text = "Already optimized!";
+                        Console.WriteLine("Already optimized!");
+                    }
+                    else
+                    {
+                        label2.Text = "Creating the file...";
+                        Console.WriteLine("Creating the file...");
+                        var stream = System.IO.File.CreateText(lastCreatedDir.FullName + "\\ClientSettings\\" + "ClientAppSettings.json");
+                        stream.Write(settings);
+                        label2.Text = "File was created!";
+                        Console.WriteLine("File was created!");
+                        stream.Close();
+                    }
+                } else
+                {
+                    label2.Text = "ClientSettings wasn\'t found! Creating it...";
+                    Console.WriteLine("ClientSettings wasn\'t found! Creating it...");
+                    System.IO.Directory.CreateDirectory(lastCreatedDir.FullName + "\\ClientSettings");
+                    var stream = System.IO.File.CreateText(lastCreatedDir.FullName + "\\ClientSettings\\" + "ClientAppSettings.json");
+                    stream.Write(settings);
+                    label2.Text = "File was created!";
+                    Console.WriteLine("File was created!");
+                    stream.Close();
+                }
             }
             else 
             {
-                Console.WriteLine("false");
-            }
+                string mainDirPath = System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\AppData\\Local\\Roblox\\Versions"; ;
+                DirectoryInfo mainDir = new DirectoryInfo(mainDirPath);
+                DirectoryInfo[] subDirs = mainDir.GetDirectories();
+                DateTime lastCreated = DateTime.MinValue;
+                DirectoryInfo lastCreatedDir = null;
+
+                label2.Text = "Finding the last version folder...";
+
+                foreach (DirectoryInfo subDir in subDirs)
+                {
+                    if (subDir.CreationTime > lastCreated)
+                    {
+                        lastCreated = subDir.CreationTime;
+                        lastCreatedDir = subDir;
+                    }
+                }
+
+                if (lastCreatedDir != null)
+                {
+                    Console.WriteLine("Last created directory: " + lastCreatedDir.FullName);
+                }
+
+                List<string> subDirsOfLastCreatedDir = new List<string>();
+                foreach (System.IO.DirectoryInfo dir in lastCreatedDir.EnumerateDirectories())
+                {
+                    subDirsOfLastCreatedDir.Add(dir.Name);
+                    // Console.WriteLine(subDirsOfLastCreatedDir.Count);
+                }
+
+                if (subDirsOfLastCreatedDir.Contains("ClientSettings"))
+                {
+                    label2.Text = "ClientSettings was found! Deleting...";
+                    Console.WriteLine("Deleting...");
+                    System.IO.Directory.Delete(lastCreatedDir.FullName + "\\ClientSettings", true);
+                    label2.Text = "Deleted!";
+                }
+                else
+                {
+                    label2.Text = "ClientSettings wasn\'t found! Everything is OK.";
+                    Console.WriteLine("ClientSettings wasn\'t found! Creating it...");
+                }
+            };
         }
     }
-}
+};
